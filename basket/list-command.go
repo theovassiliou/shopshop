3da20eff58e9fd76385f757b3735817e498ea5ba
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2020-01-29 12:55:21 +0100

    Print Listname

--- basket/list-command.go
+++ basket/list-command.go
@@ -2,11 +2,13 @@ package shopshop
 
 import (
 	"fmt"
+	"path"
+	"strings"
 )
 
 // List all items of the basket to stdout
 func (sl *Basket) List() {
-	fmt.Println("Items:")
+	fmt.Println("Items in: ", strings.TrimSuffix(path.Base(sl.FileName()), path.Ext(sl.FileName())))
 	for i, item := range sl.ShoppingList {
 		check := " "
 		if item.Done {
